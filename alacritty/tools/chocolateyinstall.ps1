e12aa1a1dbad6f169538c832fa4a99cb00cb1fb8
Jesse Miller <jmiller@jmiller.com>
2019-02-16 17:55:47 -0600

    Initial commit.

---
+++ alacritty/tools/chocolateyinstall.ps1
@@ -0,0 +1,14 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$packageName= $env:ChocolateyPackageName
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+$packageArgs = @{
+  packageName   = $packageName
+  unzipLocation = $toolsDir
+  url           = 'https://github.com/jwilm/alacritty/releases/download/v0.2.9/Alacritty-v0.2.9-windows.zip'
+  checksum      = '7edceed0ab442d746addf161a8310342c265d5ec95c23a689b13e4039f473359'
+  checksumType  = 'sha256'
+}
+
+Install-ChocolateyZipPackage @packageArgs
